7f54f7d4df29d57724345922344bc4b45c3e09b7
Moh3enzarif <mohsenzarif70@gmail.com>
2025-08-21 17:52:08 +0330

    DJ-React-Weblog

---
+++ backend/src/articles/api/views.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets
+
+from articles.models import Article
+from .serializers import ArticleSerializers
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializers
+    queryset = Article.objects.all()
+
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     DestroyAPIView,
+#     UpdateAPIView
+# )
+
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
